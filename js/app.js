fb26600e68b64ad84af14d83a90eb21e26e00af4
Lisa Chang <jofan.chang@gmail.com>
2020-07-15 20:04:17 +0800

    Update where to put bookmark id anchor

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ const fragment = document.createDocumentFragment();
 const entries = document.querySelectorAll('.entry');
 const navList = document.querySelector('nav');
 for (const entry of entries) {
+  const entryText = entry.textContent;
+  const entryBox = entry.closest("section");
+  entryBox.setAttribute("id", entryText);
   const newElement = document.createElement('li');
-  newElement.textContent = entry.textContent;
+  newElement.innerHTML = `<a href="#${entryText}"">${entryText}</a>`;
   fragment.appendChild(newElement)
 };
 navList.appendChild(fragment);
