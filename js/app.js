d9a527c2401da291ceaff8aef143dadde1437267
Lisa Chang <jofan.chang@gmail.com>
2020-07-15 23:07:20 +0800

    Add addLoadEvent function

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-const fragment = document.createDocumentFragment();
-const entries = document.querySelectorAll('.entry');
-const navList = document.querySelector('nav');
-for (const entry of entries) {
-  const entryText = entry.textContent;
-  const entryBox = entry.closest("section");
-  entryBox.setAttribute("id", entryText);
-  const newElement = document.createElement('li');
-  newElement.innerHTML = `<a href="#${entryText}"">.</a>`;
-  fragment.appendChild(newElement)
+function prepareNavigation() {
+  const fragment = document.createDocumentFragment();
+  const entries = document.querySelectorAll('.entry');
+  const navList = document.querySelector('nav');
+  for (const entry of entries) {
+    const entryText = entry.textContent;
+    const entryBox = entry.closest("section");
+    entryBox.setAttribute("id", entryText);
+    const newElement = document.createElement('li');
+    newElement.innerHTML = `<a href="#${entryText}"">.</a>`;
+    fragment.appendChild(newElement)
+  };
+  navList.appendChild(fragment);
 };
-navList.appendChild(fragment);
+
+function addLoadEvent(func){
+  const oldonload = window.onload;
+  if (typeof window.onload != "function") {
+    window.onload = func;
+  } else {
+    window.onload = function(){
+      oldonload();
+      func();
+    };
+  };
+};
+
+addLoadEvent(prepareNavigation);
