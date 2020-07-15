5bb8fef6e48ab9a796814f72d9f3a9dd2b8a586b
Lisa Chang <jofan.chang@gmail.com>
2020-07-15 23:43:25 +0800

    Remove nav tag in html
    Update how navigation is prepared: fully created in js rather than having nav and ul tags in html

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 function prepareNavigation() {
-  const fragment = document.createDocumentFragment();
   const entries = document.querySelectorAll('.entry');
-  const navList = document.querySelector('nav');
+  const navList = document.createElement('nav');
+  const uList = document.createElement('ul');
+  const mainSpot = document.querySelector('main');
   for (const entry of entries) {
     const entryText = entry.textContent;
     const entryBox = entry.closest("section");
     entryBox.setAttribute("id", entryText);
     const newElement = document.createElement('li');
     newElement.innerHTML = `<a href="#${entryText}"">.</a>`;
-    fragment.appendChild(newElement)
+    uList.appendChild(newElement)
   };
-  navList.appendChild(fragment);
+  navList.appendChild(uList);
+  document.body.insertBefore(navList, mainSpot);
+  navList.addEventListener('click', respondClick);
 };
+
+function respondClick(evt) {
+  evt.preventDefault();
+  evt.target.scrollIntoView({behavior: "smooth"});
+}
 
 function addLoadEvent(func){
   const oldonload = window.onload;
